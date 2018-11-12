2ce2c82e14269bb6d647eaed1f76b7ed94b9784f
Mark <mark@iMac2.local>
2018-11-12 08:26:00 -0500

    added conversion function

--- unit_eight_warmups.py
+++ unit_eight_warmups.py
@@ -3,6 +3,13 @@ import tkinter
 root = tkinter.Tk()
 root.title("Temperature Converter")
 
+
+def f_to_c():
+    value = int(f_value.get())
+    c = 5/9 * (value - 32)
+    c_value.set(str(c))
+
+
 f_value = tkinter.StringVar()
 
 c_value = tkinter.StringVar()
@@ -15,5 +22,11 @@ fahrenheit.grid(row=1, column=1)
 
 fahrenheit_two = tkinter.Entry(root, textvariable=f_value)
 fahrenheit_two.grid(row=1, column=2)
+
+celsius_two = tkinter.Label(root, textvariable=c_value)
+celsius_two.grid(row=2, column=2)
+
+convert = tkinter.Button(root, text="convert", command=f_to_c)
+convert.grid(row=3, column=1)
 
 root.mainloop()
