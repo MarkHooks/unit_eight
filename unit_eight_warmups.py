ec1fa673435c5a3730aed92b85b9fc5bc9a4a05d
Mark <mark@iMac2.local>
2018-11-13 09:11:26 -0500

    Clean up GUI

--- unit_eight_warmups.py
+++ unit_eight_warmups.py
@@ -14,6 +14,8 @@ f_value = tkinter.StringVar()
 
 c_value = tkinter.StringVar()
 
+f_value.set("0")
+
 celsius = tkinter.Label(root, text="degrees C:")
 celsius.grid(row=2, column=1)
 
@@ -24,9 +26,9 @@ fahrenheit_two = tkinter.Entry(root, textvariable=f_value)
 fahrenheit_two.grid(row=1, column=2)
 
 celsius_two = tkinter.Label(root, textvariable=c_value)
-celsius_two.grid(row=2, column=2)
+celsius_two.grid(row=2, column=2, sticky="W")
 
 convert = tkinter.Button(root, text="convert", command=f_to_c)
-convert.grid(row=3, column=1)
+convert.grid(row=3, column=1, columnspan=2)
 
 root.mainloop()
